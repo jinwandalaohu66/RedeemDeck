7a4f4e23c47a402c98fb27235d679758414e6ca9
Matteo Comisso <matteo.comisso@me.com>
2025-12-08 16:27:26 +0000

    Initial Commit

---
+++ AppStoreCodesTests/AppStoreCodesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCodesTests.swift
+//  AppStoreCodesTests
+//
+//  Created by Matteo Comisso on 08/12/2025.
+//
+
+import Testing
+@testable import AppStoreCodes
+
+struct AppStoreCodesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
